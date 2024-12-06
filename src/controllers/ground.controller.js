4565bb4422f5c681e70d0508a218949780636b8b
kishortalukdar <kishortalukdar55@gmail.com>
2024-12-06 15:45:41 +0530

    imgaes issue fix on ground list

--- src/controllers/ground.controller.js
+++ src/controllers/ground.controller.js
@@ -17,82 +17,81 @@ export const createGround = asyncHandler(async (req, res) => {
 });
 
 export const getAllGrounds = asyncHandler(async (req, res) => {
-
-    const { gameType } = req.query
-
+    const { gameType } = req.query;
+  
     try {
-        const groundList = await Ground.aggregate([
-            {
-                $lookup: {
-                    from: 'gametypes',
-                    localField: 'gameTypeId',
-                    foreignField: '_id',
-                    as: 'gameType',
+      const groundList = await Ground.aggregate([
+        {
+          $lookup: {
+            from: 'gametypes',
+            localField: 'gameTypeId',
+            foreignField: '_id',
+            as: 'gameType',
+          },
+        },
+        {
+          $unwind: {
+            path: '$gameType',
+            preserveNullAndEmptyArrays: true, // Ensures documents are not removed if gameType is null/empty
+          },
+        },
+        {
+          $match: {
+            ...(gameType && gameType !== 'all' ? { 'gameType.name': gameType } : {}),
+          },
+        },
+        {
+          $lookup: {
+            from: 'gamefeatures',
+            localField: 'gameFeaturesId',
+            foreignField: '_id',
+            as: 'gameFeatures',
+          },
+        },
+        {
+          $unwind: {
+            path: '$gameFeatures',
+            preserveNullAndEmptyArrays: true, // Ensures documents are not removed if gameFeatures is null/empty
+          },
+        },
+        {
+          $project: {
+            name: 1,
+            description: 1,
+            lat: 1,
+            lng: 1,
+            city: 1,
+            state: 1,
+            country: 1,
+            address: 1,
+            image: {
+                $cond: {
+                  if: { $isArray: '$image' },
+                  then: { $arrayElemAt: ['$image', 0] },
+                  else: '$image', 
                 },
-            },
-            {
-                $unwind: {
-                    path: '$gameType',
-                    preserveNullAndEmptyArrays: true, // Ensures documents are not removed if gameType is null/empty
-
-                },
-            },
-            {
-                $match: {
-                    ...(gameType && gameType !== "all" ? { 'gameType.name': gameType } : {}),
-                },
-              },
-            {
-                $lookup: {
-                    from: 'gamefeatures',
-                    localField: 'gameFeaturesId',
-                    foreignField: '_id',
-                    as: 'gameFeatures',
-                },
-            },
-            {
-                $unwind: {
-                    path: '$gameFeatures',
-                    preserveNullAndEmptyArrays: true, // Ensures documents are not removed if gameType is null/empty
-
-                },
-            },
-            {
-                $project : {
-                    name : 1,
-                    description : 1,
-                    lat : 1,
-                    lng : 1,
-                    city : 1,
-                    state : 1,
-                    country : 1,
-                    address : 1,
-                    image : 1,
-                    index : 1,
-                    likes : 1,
-                    equipmentProvide : 1,
-                    toilet : 1,
-                    changingRoom : 1,
-                    parking : 1,
-                    showers : 1,
-                  
-                }
-            }
-        ])
-
-        
-
-        if(groundList.length === 0){
-            return res.status(404).json(new ApiResponse(404, '', 'No Data found'));
-
-        }
-        return res.status(200).json(new ApiResponse(200, groundList));
-
+              },           
+            index: 1,
+            likes: 1,
+            equipmentProvide: 1,
+            toilet: 1,
+            changingRoom: 1,
+            parking: 1,
+            showers: 1,
+          },
+        },
+      ]);
+  
+      if (groundList.length === 0) {
+        return res.status(404).json(new ApiResponse(404, '', 'No Data found'));
+      }
+      return res.status(200).json(new ApiResponse(200, groundList));
     } catch (error) {
-        console.log(error);
-        return res.status(500).json(new ApiResponse(500, '', 'Server Error'));
+      console.log(error);
+      return res.status(500).json(new ApiResponse(500, '', 'Server Error'));
     }
-});
+  });
+  
 export const getGroundDetails = asyncHandler(async (req, res) => {
     const { _id } = req.params;
 
